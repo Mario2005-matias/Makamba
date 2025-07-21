c9153debdbedb8bb047b9032bcb6726c82706b7c
matiasDev007 <mariomatias879@gmail.com>
2025-07-21 19:12:27 +0100

    primeiro commit

---
+++ makamba/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     <h1 className="bg-red-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error neque vitae minus autem eos deserunt unde optio, amet, totam quae delectus aliquam sequi. Ipsam, consectetur eligendi alias facere distinctio recusandae.</h1>
+    </>
+  )
+}
+
+export default App
