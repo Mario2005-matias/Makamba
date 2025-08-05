832b283afdfe959b72a9e948c9de132073602890
Filipe Fernandes <kidscript123@gmail.com>
2025-08-05 16:26:13 -0400

    Resolvendo Bug de Responsividade secction Historia

--- makamba/src/components/sobre/Visao.tsx
+++ makamba/src/components/sobre/Visao.tsx
@@ -1,7 +1,7 @@
 export default function Visao(){
 
     return(
-        <div className="bg-gray-900 text-white mb-10 px-6 py-4 flex justify-center gap-30 flex-wrap items-center text-sm text-gray-300">
+        <div className="bg-gray-900 text-white mb-10 px-6 py-4 flex justify-center flex-wrap items-center text-sm text-gray-300">
             <div className="flex flex-col items-start w-120 px-6 py-4 ">
                 <h1 className="py-5 text-4xl">Makamba <span className="text-[#FF6700] ">Tec</span></h1>
                 <p className="text-lg">
@@ -13,7 +13,7 @@ export default function Visao(){
                 </p>
             </div>
 
-            <div className="flex flex-wrap bg-red-500 justify-center items-center w-120">
+            <div className="flex flex-wrap grid grid-cols-2 justify-center items-center w-190">
                 <div className=" p-5 m-10 text-lg text-[#FF6700] flex flex-col items-center px-6 py-4  ">
                     <h1>Nosso Obejctivo</h1>
                     <p className="text-sm text-white text-justify"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis obcaecati a vero minima aperiam esse, tempora labore possimus quae. Consequuntur harum excepturi blanditiis nobis. Facere sapiente nam hic eum optio!</p>
